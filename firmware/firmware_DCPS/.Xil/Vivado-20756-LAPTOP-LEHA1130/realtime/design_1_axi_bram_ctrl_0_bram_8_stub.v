2fc55f062eede19669a4041701cb60490f5c36ab
akshaynaik16 <51712114+akshaynaik16@users.noreply.github.com>
2021-12-09 17:58:04 -0600

    Created TCL Folder

---
+++ firmware/firmware_DCPS/.Xil/Vivado-20756-LAPTOP-LEHA1130/realtime/design_1_axi_bram_ctrl_0_bram_8_stub.v
@@ -0,0 +1,24 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "blk_mem_gen_v8_4_4,Vivado 2019.2" *)
+module design_1_axi_bram_ctrl_0_bram_8(clka, rsta, ena, wea, addra, dina, douta, clkb, rstb, enb, 
+  web, addrb, dinb, doutb, rsta_busy, rstb_busy);
+  input clka;
+  input rsta;
+  input ena;
+  input [31:0]wea;
+  input [31:0]addra;
+  input [255:0]dina;
+  output [255:0]douta;
+  input clkb;
+  input rstb;
+  input enb;
+  input [31:0]web;
+  input [31:0]addrb;
+  input [255:0]dinb;
+  output [255:0]doutb;
+  output rsta_busy;
+  output rstb_busy;
+endmodule
